203932261fdc3a683c9eae4c03bc5feeb9592bdf
Hunt3r2 <151632355+Hunt3r2@users.noreply.github.com>
2026-04-18 18:49:28 +0200

    Se ha implementado el apartado de riesgos
    falta funcionalidad

--- assets/js/tailwind-config.js
+++ assets/js/tailwind-config.js
@@ -1,67 +1,69 @@
-tailwind.config = {
-            darkMode: "class",
-            theme: {
-                extend: {
-                    colors: {
-                        "on-tertiary-fixed": "#3b0900",
-                        "inverse-surface": "#2e3038",
-                        "inverse-primary": "#b2c5ff",
-                        "on-tertiary-fixed-variant": "#872100",
-                        "tertiary-fixed": "#ffdbd1",
-                        "on-surface": "#191b22",
-                        "on-primary": "#ffffff",
-                        "surface-bright": "#faf8ff",
-                        "on-error-container": "#93000a",
-                        "primary-container": "#0056d2",
-                        "on-error": "#ffffff",
-                        "primary": "#0040a1",
-                        "secondary": "#006a6a",
-                        "surface-container-lowest": "#ffffff",
-                        "primary-fixed": "#dae2ff",
-                        "primary-fixed-dim": "#b2c5ff",
-                        "on-primary-fixed-variant": "#0040a1",
-                        "secondary-fixed": "#8cf3f3",
-                        "tertiary-container": "#b12f00",
-                        "inverse-on-surface": "#f0f0fa",
-                        "on-primary-container": "#ccd8ff",
-                        "surface-variant": "#e1e2ec",
-                        "on-tertiary-container": "#ffcfc1",
-                        "secondary-fixed-dim": "#6fd7d6",
-                        "on-primary-fixed": "#001847",
-                        "surface-container-high": "#e7e7f1",
-                        "surface-tint": "#0056d2",
-                        "on-background": "#191b22",
-                        "on-secondary": "#ffffff",
-                        "on-secondary-fixed-variant": "#004f4f",
-                        "on-secondary-fixed": "#002020",
-                        "tertiary-fixed-dim": "#ffb5a0",
-                        "tertiary": "#882200",
-                        "surface-container": "#ededf7",
-                        "surface-container-low": "#f2f3fd",
-                        "surface-dim": "#d9d9e3",
-                        "background": "#faf8ff",
-                        "surface-container-highest": "#e1e2ec",
-                        "error": "#ba1a1a",
-                        "surface": "#faf8ff",
-                        "on-tertiary": "#ffffff",
-                        "outline": "#737785",
-                        "outline-variant": "#c3c6d6",
-                        "on-surface-variant": "#424654",
-                        "error-container": "#ffdad6",
-                        "secondary-container": "#8cf3f3",
-                        "on-secondary-container": "#007070"
-                    },
-                    borderRadius: {
-                        DEFAULT: "0.25rem",
-                        lg: "0.5rem",
-                        xl: "0.75rem",
-                        full: "9999px"
-                    },
-                    fontFamily: {
-                        headline: ["Manrope"],
-                        body: ["Public Sans"],
-                        label: ["Public Sans"]
-                    }
-                }
-            }
-        }
+window.tailwind = window.tailwind || {};
+
+window.tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "on-tertiary-fixed": "#3b0900",
+        "inverse-surface": "#2e3038",
+        "inverse-primary": "#b2c5ff",
+        "on-tertiary-fixed-variant": "#872100",
+        "tertiary-fixed": "#ffdbd1",
+        "on-surface": "#191b22",
+        "on-primary": "#ffffff",
+        "surface-bright": "#faf8ff",
+        "on-error-container": "#93000a",
+        "primary-container": "#0056d2",
+        "on-error": "#ffffff",
+        "primary": "#0040a1",
+        "secondary": "#006a6a",
+        "surface-container-lowest": "#ffffff",
+        "primary-fixed": "#dae2ff",
+        "primary-fixed-dim": "#b2c5ff",
+        "on-primary-fixed-variant": "#0040a1",
+        "secondary-fixed": "#8cf3f3",
+        "tertiary-container": "#b12f00",
+        "inverse-on-surface": "#f0f0fa",
+        "on-primary-container": "#ccd8ff",
+        "surface-variant": "#e1e2ec",
+        "on-tertiary-container": "#ffcfc1",
+        "secondary-fixed-dim": "#6fd7d6",
+        "on-primary-fixed": "#001847",
+        "surface-container-high": "#e7e7f1",
+        "surface-tint": "#0056d2",
+        "on-background": "#191b22",
+        "on-secondary": "#ffffff",
+        "on-secondary-fixed-variant": "#004f4f",
+        "on-secondary-fixed": "#002020",
+        "tertiary-fixed-dim": "#ffb5a0",
+        "tertiary": "#882200",
+        "surface-container": "#ededf7",
+        "surface-container-low": "#f2f3fd",
+        "surface-dim": "#d9d9e3",
+        "background": "#faf8ff",
+        "surface-container-highest": "#e1e2ec",
+        "error": "#ba1a1a",
+        "surface": "#faf8ff",
+        "on-tertiary": "#ffffff",
+        "outline": "#737785",
+        "outline-variant": "#c3c6d6",
+        "on-surface-variant": "#424654",
+        "error-container": "#ffdad6",
+        "secondary-container": "#8cf3f3",
+        "on-secondary-container": "#007070"
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px"
+      },
+      fontFamily: {
+        headline: ["Manrope"],
+        body: ["Public Sans"],
+        label: ["Public Sans"]
+      }
+    }
+  }
+};
